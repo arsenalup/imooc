2f2699ab9661c734cfa14625ca8ec47cdab039b8
arsenalup <676250063@qq.com>
2017-07-19 23:54:21 +0800

    7.19

--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name='昵称', default='')
     birday = models.DateField(verbose_name='生日', null=True, blank=True)
-    gender = models.CharField(max_length=15, choices=(('male', '男'), ('female', '女')), default='famale')
+    gender = models.CharField(max_length=15, choices=(('male', '男'), ('female', '女')), default='female')
     address = models.CharField(max_length=100, default='')
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png', max_length=100)
@@ -24,7 +24,7 @@ class UserProfile(AbstractUser):
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name='验证码')
     email = models.EmailField(max_length=50, verbose_name='邮箱')
-    send_type = models.CharField(choices=(('register','注册'),('forget', '找回密码')), max_length=10, verbose_name='验证码类型')
+    send_type = models.CharField(choices=(('register','注册'),('forget', '找回密码'),('update_email', '修改邮箱')), max_length=20, verbose_name='验证码类型')
     send_time = models.DateTimeField(default=datetime.now, verbose_name='发送时间')
 
     class Meta:
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import url, include
 
-from .views import UserInfoView
+from .views import UserInfoView, UploadImageView, UpdatePwdView, SendEmailCodeView, UpdateEmailView
 
 urlpatterns = [
     #用户信息
     url(r'^info/$', UserInfoView.as_view(), name='user_info'),
 
+    #用户头像上传
+    url(r'^image/upload/$', UploadImageView.as_view(), name='image_upload'),
+
+    #用户个人中心修改密码
+    url(r'^update/pwd/$', UpdatePwdView.as_view(), name='update_pwd'),
+
+    #发送邮箱验证码
+    url(r'^sendemail_code/$', SendEmailCodeView.as_view(), name='sendemail_code'),
+
+    # 修改邮箱
+    url(r'^update_email/$', UpdateEmailView.as_view(), name='update_email'),
 
 ]
